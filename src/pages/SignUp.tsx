1748ff16d4266cc1213027e332a6034ad328770a
MistbornL <lashaloria3@gmail.com>
2022-11-23 15:19:54 +0400

    register stage 1

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,9 @@
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import React from "react";
 
 export const SignUp = () => {
@@ -14,76 +20,56 @@ export const SignUp = () => {
                       Sign up
                     </p>
 
-                    <form className="mx-1 mx-md-4">
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <form className="mx-1 mx-md-4 ">
+                      <div className="d-flex flex-row  align-items-center gap-2 mb-4">
+                        <FontAwesomeIcon size="lg" icon={faUser} />
                         <div className="form-outline flex-fill mb-0">
                           <input
                             type="text"
                             id="form3Example1c"
                             className="form-control"
+                            placeholder="Your Name"
                           />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example1c"
-                          >
-                            Your Name
-                          </label>
                         </div>
                       </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                      <div className="d-flex flex-row align-items-center gap-2 mb-4">
+                        <FontAwesomeIcon size="lg" icon={faEnvelope} />
                         <div className="form-outline flex-fill mb-0">
                           <input
                             type="email"
                             id="form3Example3c"
+                            placeholder="Your Email"
                             className="form-control"
                           />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example3c"
-                          >
-                            Your Email
-                          </label>
                         </div>
                       </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                      <div className="d-flex flex-row align-items-center gap-2 mb-4">
+                        <FontAwesomeIcon size="lg" icon={faLock} />
                         <div className="form-outline flex-fill mb-0">
                           <input
                             type="password"
                             id="form3Example4c"
                             className="form-control"
+                            placeholder="Password"
                           />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example4c"
-                          >
-                            Password
-                          </label>
                         </div>
                       </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                      <div className="d-flex flex-row align-items-center gap-2 mb-4">
+                        <FontAwesomeIcon size="lg" icon={faKey} />
                         <div className="form-outline flex-fill mb-0">
                           <input
                             type="password"
                             id="form3Example4cd"
                             className="form-control"
+                            placeholder="Repeat your password"
                           />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example4cd"
-                          >
-                            Repeat your password
-                          </label>
                         </div>
                       </div>
 
-                      <div className="form-check d-flex justify-content-center mb-5">
+                      <div className="form-check d-flex justify-content-center gap-2 mb-5">
                         <input
                           className="form-check-input me-2"
                           type="checkbox"
